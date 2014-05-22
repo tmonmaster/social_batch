3572b401148d075ed48311c4b1ddaa4fd2a9154d
u_kino07 <u_kino07@tmon-u_kino07.tmoncorp.com>
2014-05-22 17:02:58 +0900

    [지라번호][강이경][뉴스 수집 어플리케이션]
    소스 리펙토링
       - KeywordDao.java
       - Keyword.java
       - KeywordMapper.xml

--- social-batch/src/main/java/kr/co/tmon/social/batch/dao/KeywordDao.java
+++ social-batch/src/main/java/kr/co/tmon/social/batch/dao/KeywordDao.java
@@ -7,25 +7,21 @@ import kr.co.tmon.social.batch.vo.Keyword;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
 /**
  * 
  * @author 강이경
  * 
  * DB의 정보를 Keyword 객체에 가져오는 클래스
- *
+ * 
  */
 @Repository
 public class KeywordDao {
+	private static final String	KEYWORD_MAPPER	= "kr.co.tmon.social.batch.dao.mapper.Keyword.";
 	@Autowired
-	private SqlSession	sqlSession;
+	private SqlSession sqlSession;
 
-	/**
-	 * ighgggghkgkjgjgkh
-	 * 커밋 테스트
-	 * 테스트2
-	 * @return
-	 */
 	public List<Keyword> getKeywordList() {
-		return sqlSession.selectList("kr.co.tmon.social.batch.vo.Keyword.getKeyword");
+		return sqlSession.selectList(KEYWORD_MAPPER + "getKeyword");
 	}
 }

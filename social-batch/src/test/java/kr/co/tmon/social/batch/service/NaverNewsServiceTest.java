8c59586fcdf888f82d6059742b5414b8da9c3928
tmonmaster <forfle@daum.net>
2014-05-29 22:06:43 +0900

    [NEWOJT-32][이윤호][네이버 뉴스 RSS 파싱]
    실제 KeywordDao를 사용하여 파싱 테스트 했던 것을 Mock객체로 만들어서 테스트하는 것으로 변경.

--- social-batch/src/test/java/kr/co/tmon/social/batch/service/NaverNewsServiceTest.java
+++ social-batch/src/test/java/kr/co/tmon/social/batch/service/NaverNewsServiceTest.java
@@ -1,36 +1,53 @@
 package kr.co.tmon.social.batch.service;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import kr.co.tmon.social.batch.dao.KeywordDao;
+import kr.co.tmon.social.batch.vo.Keyword;
 import kr.co.tmon.social.batch.vo.News;
 
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 /**
  * 
  * @author Yunho Lee
  * 
- * 네이버 뉴스 RSS 파싱 테스트
+ *         네이버 뉴스 RSS 파싱 테스트
  * 
  */
 
+@RunWith(MockitoJUnitRunner.class)
 public class NaverNewsServiceTest {
+
+	@Mock
+	private KeywordDao keywordDao;
+
+	@InjectMocks
+	private NaverNewsService naverNewsService;
 
 	@Test
 	public void parsingTest() throws Exception {
-		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring/applicationContext.xml");
-		NaverNewsService naverNewsService = applicationContext.getBean("naverNewsService", NaverNewsService.class);
+		List<Keyword> keywordList = new ArrayList<>();
+		keywordList.add(new Keyword("티켓몬스터", "티몬"));
+		keywordList.add(new Keyword("티켓몬스터", "티켓몬스터"));
+		keywordList.add(new Keyword("쿠팡", "쿠팡"));
+		keywordList.add(new Keyword("위메프", "위메프"));
+
+		when(keywordDao.getKeywordList()).thenReturn(keywordList);
 
 		List<News> naverNewsList = naverNewsService.getNewsList();
 
-		assertNotNull(naverNewsList);
+		assertNotSame(0, naverNewsList.size());
 
 		for (int i = 0; i < naverNewsList.size(); i++)
 			System.out.println(naverNewsList.get(i).toString());
-
 	}
 }

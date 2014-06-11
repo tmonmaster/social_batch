b2532a9ddf7791fa90b07051c779c21e76466b5b
u_kino07 <u_kino07@tmon-u_kino07.tmoncorp.com>
2014-06-11 09:16:24 +0900

    [NEWOJT-140][필터링 기능 구현 Service 생성 - 단독 뉴스일 경우 가산점 부가][강이경]
    SingleNewsFilteringService.java 생성
    단독 뉴스 기사일 경우만 relationScore에 가점 부여 기능 구현

---
+++ social-batch/src/main/java/kr/co/tmon/social/filter/service/SingleNewsFilteringService.java
@@ -0,0 +1,38 @@
+package kr.co.tmon.social.filter.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kr.co.tmon.social.filter.dao.NewsForFilteringDao;
+import kr.co.tmon.social.filter.vo.NewsForFiltering;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * 단독 기사일 경우, 연관성에 가점을 준다.
+ * 
+ * @author 강이경
+ * 
+ */
+
+@Service
+public class SingleNewsFilteringService {
+	public final static int ADDITIONAL_RELATION_SCORE = 5;
+
+	@Autowired
+	private NewsForFilteringDao newsForFilteringDao;
+
+	List<NewsForFiltering> getSingleNewsListBy(String date) throws Exception {
+		List<NewsForFiltering> singleNewsList = new ArrayList<NewsForFiltering>();
+		singleNewsList = newsForFilteringDao.getSingleNewsForFilteringList(date);
+		return singleNewsList;
+	}
+
+	void applyAdditionalRelationScore(List<NewsForFiltering> singleNewsList) {
+		for (NewsForFiltering singleNews : singleNewsList) {
+			singleNews.setRelationScore(singleNews.getRelationScore() + ADDITIONAL_RELATION_SCORE);
+		}
+		newsForFilteringDao.updateRelationScoreList(singleNewsList);
+	}
+}
